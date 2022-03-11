67c00796cddb4372db033c75674d46546ca74dfe
dkotenko <dmitrii.kotenko@gmail.com>
2022-03-11 11:45:54 +0300

    parser.h refreshed

--- includes/parser.h
+++ includes/parser.h
@@ -1,9 +1,6 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-
-
-
 typedef struct {
 	float x;
 	float y;
@@ -15,24 +12,38 @@ typedef struct {
 	float z;
 } t_vec3;
 
-int (*length)(void);
-
+typedef struct {
+	int vertex;
+	int texture;
+	int normal;
+} t_vx_index;
 
 typedef struct {
-	
-} t_vertex;
+	t_vx_index *vxs;
+	int vx_num;
+} t_face;
 
 typedef struct {
-
+	t_vec3 *vertices;
+	t_vec2 *normals;
+	t_vec3 *textures;
+	t_face *faces;
+	int material_index;
 } t_mesh;
 
 typedef struct {
 	t_obj_node *children;
-	t_mesh *meshes;
+	int *meshes_indexes;
 } t_obj_node;
 
 typedef struct {
-	
+	int (*f)(void);
+} t_material;
+
+typedef struct {
+	t_obj_node *root_node;
+	t_mesh *meshes;
+	t_material *materials;
 } t_obj;
 
 #endif
